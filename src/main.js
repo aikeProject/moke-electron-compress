1bcc7bc848fbbd343ea6ad40ab48e125a94a755c
成雨 <rensailong@zhongan.io>
2019-11-28 16:34:43 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,58 @@
+/**
+ * @author 成雨
+ * @date 2019/11/27
+ * @Description:
+ */
+
+const { app, ipcMain } = require('electron');
+const dev = require('electron-is-dev');
+const AppWindow = require('./AppWindow');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+    app.quit();
+}
+
+let mainWindow;
+
+const createWindow = () => {
+    // Create the browser window.
+    mainWindow = new AppWindow({
+        width: 300,
+        height: 600,
+    }, `file://${path.join(__dirname, './index.html')}`);
+
+    // Open the DevTools.
+    dev && mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
---
+++ src/renderer.js
@@ -0,0 +1,234 @@
+/**
+ * @author 成雨
+ * @date 2019/11/27
+ * @Description:
+ */
+
+const {ipcRenderer, remote} = require('electron');
+const path = require('path');
+const sharp = require('sharp');
+const uuidV4 = require('uuid/v4');
+
+const versionNode = process.versions.node;
+const versionElectron = process.versions.electron;
+
+console.log('渲染进程...');
+console.log(`👋 node version ${versionNode}`);
+console.log(`👋 electron version ${versionElectron}`);
+
+let filesMap = {};
+let compressing = false;
+
+const flattenArr = (arr) => {
+    return arr.reduce((map, item) => {
+        map[item.id] = item;
+        return map
+    }, {})
+};
+
+const objToArr = (obj) => {
+    return Object.keys(obj).map(key => obj[key]);
+};
+
+function flattenSize(size) {
+    if (size > 1024 * 1024) return `${(size / 1024 / 1024).toFixed(2)}MB`;
+
+    return `${(size / 1024).toFixed(2)}kb`
+}
+
+function filterSource(source) {
+    const fileType = /image\/(png|jpg|jpeg)/;
+    let files = [].slice.apply(source);
+
+    files = files.filter(file => {
+        return fileType.test(file.type)
+    });
+
+    files = files.map(file => ({
+        id: uuidV4().split('-').join(''),
+        name: file.name,
+        path: file.path,
+        size: file.size,
+        type: file.type.split('/')[1],
+        compress: false,
+        compressSize: null,
+        done: false,
+        error: false
+    }));
+
+    filesMap = {
+        ...filesMap,
+        ...flattenArr(files)
+    };
+
+    return filesMap
+}
+
+function render(files) {
+
+    files = objToArr(files);
+    const listGroup = document.querySelector('#list-group');
+    let str = '';
+
+    if (!files.length) return [];
+
+    document.querySelector('.background-drop').style.display = 'none';
+
+    files.forEach(file => {
+
+        const done = file.done ? 'bg-success' : 'progress-bar-striped';
+        const pending = file.compress
+            ? 'progress-bar-animated bg-success' :
+            file.done ? '' : 'bg-warning';
+        const progress = `progress-bar ${done} ${pending}`;
+
+        console.log('file', file);
+
+        str += `<li class="list-group-item">
+                    <div class="name ellipsis">${file.name}</div>
+                    <div class="size">
+                        <div class="size--pending">${flattenSize(file.size)}</div>
+                        <div class="size--success">${file.compressSize ? flattenSize(file.compressSize) : ''}</div>
+                    </div>
+                    <div class="progress">
+                        <div class="${progress}"></div>
+                    </div>
+                </li>`;
+    });
+
+    listGroup.innerHTML = str;
+
+    // 按钮状态
+    compressBtn()
+}
+
+document.querySelector('.background-drop').addEventListener('click', () => {
+    document.querySelector('#select-files').click();
+    document.querySelector('#select-files').addEventListener('change', (e) => {
+        const files = filterSource(e.target.files);
+        render(files);
+    })
+});
+
+function compressInfo(file) {
+    return (info) => {
+        filesMap = {
+            ...filesMap,
+            [file.id]: {
+                ...file,
+                compressSize: info.size,
+                compress: false,
+                done: true,
+            }
+        };
+
+        compressing = compressDone();
+
+        render(filesMap);
+    }
+}
+
+function compressError(file) {
+    return (err) => {
+        filesMap = {
+            ...filesMap,
+            [file.id]: {
+                ...file,
+                error: true
+            }
+        };
+        compressing = false;
+        render(filesMap);
+    }
+}
+
+function compressDone() {
+    return !Object.values(filesMap).every(file => file.done);
+}
+
+function compressBtn() {
+    const compress = document.querySelector('#compress');
+    if (compressing) compress.setAttribute('disabled', 'true');
+    else compress.removeAttribute('disabled')
+}
+
+function compress(files) {
+
+    if (compressing) return;
+    compressing = true;
+
+    const desktop = remote.app.getPath('desktop');
+    const defaultOpt = {
+        quality: 50,
+        chromaSubsampling: '4:4:4'
+    };
+    files = objToArr(files);
+
+    if (!files.length) return;
+
+    files.map(file => {
+        let fileData = sharp(file.path);
+
+        const type = file.type;
+
+        if (!type) return;
+
+        switch (type) {
+            case "png":
+                fileData = fileData
+                    .png(defaultOpt)
+                    .toFile(path.join(desktop, file.name))
+                    .then(compressInfo(file))
+                    .catch(compressError(file));
+                break;
+            case "jpg":
+                fileData = fileData
+                    .jpg(defaultOpt)
+                    .toFile(path.join(desktop, file.name))
+                    .then(compressInfo(file))
+                    .catch(compressError(file));
+                break;
+            case "jpeg":
+                fileData
+                    .jpeg(defaultOpt)
+                    .toFile(path.join(desktop, file.name))
+                    .then(compressInfo(file))
+                    .catch(compressError(file));
+                break;
+            default:
+                break;
+        }
+
+    });
+
+}
+
+document.querySelector('#compress').addEventListener('click', () => {
+
+    Object.values(filesMap).forEach(file => {
+        filesMap[file.id] = {...file, compress: true}
+    });
+
+    render(filesMap);
+
+    compress(filesMap)
+});
+
+function handleDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    const files = filterSource(e.dataTransfer.files);
+    render(files)
+
+}
+
+function handleDragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+}
+
+document.addEventListener('dragenter', handleDragover, false);
+document.addEventListener('dragover', handleDragover, false);
+document.addEventListener('drop', handleDrop, false);
