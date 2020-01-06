0d15f833a3ce147c29e843a7b301731596362355
成雨 <rensailong@zhongan.io>
2020-01-06 20:01:46 +0800

    update

--- preinstall.sh
+++ preinstall.sh
@@ -24,30 +24,31 @@ if [[ -z $NO_LOCAL ]]; then
 
     # 换源安装sharp
     export SHARP_DIST_BASE_URL="https://npm.taobao.org/mirrors/sharp-libvips/v8.8.1/"
+
+
+    npm i electron@${electron} -D -E
+
+    if [[ `command -v python` ]];then
+        echo 'python 已经安装...'
+        echo "安装的python版本为：$(python --version)"
+        printf "\n"
+    else
+       echo '需要安装python...'
+    fi
+
+    # node-gyp 工具重新编译sharp，以适应当前版本electron
+    # https://electronjs.org/docs/tutorial/using-native-node-modules#%E4%B8%BA-electron-%E6%89%8B%E5%8A%A8%E7%BC%96%E8%AF%91
+    npm i node-gyp@5.0.7 -D -E
+
+#    npm i prebuild-install@5.3.3 -g -E
+
+    npm i sharp@0.23.4 -D -E
+
+    echo "重新编译sharp"
+    cd node_modules/sharp
+    echo "切换到sharp目录下: $(pwd)"
+    echo "npx node-gyp rebuild --target=${electron} --arch=x64 --dist-url=https://electronjs.org/headers"
+    #export HOME="~/.electron-gyp"
+    npx node-gyp rebuild --target=${electron} --arch=x64 --dist-url=https://electronjs.org/headers
 fi
-
-npm i electron@${electron} -D -E
-
-if [[ `command -v python` ]];then
-    echo 'python 已经安装...'
-    echo "安装的python版本为：$(python --version)"
-    printf "\n"
-else
-   echo '需要安装python...'
-fi
-
-# node-gyp 工具重新编译sharp，以适应当前版本electron
-# https://electronjs.org/docs/tutorial/using-native-node-modules#%E4%B8%BA-electron-%E6%89%8B%E5%8A%A8%E7%BC%96%E8%AF%91
-#npm i node-gyp@5.0.7 -D -E
-#
-#npm i prebuild-install@5.3.3 -g -E
-#
-#npm i sharp@0.23.4 -D -E
-#
-#echo "重新编译sharp"
-#cd node_modules/sharp
-#echo "切换到sharp目录下: $(pwd)"
-#echo "npx node-gyp rebuild --target=${electron} --arch=x64 --dist-url=https://electronjs.org/headers"
-##export HOME="~/.electron-gyp"
-#npx node-gyp rebuild --target=${electron} --arch=x64 --dist-url=https://electronjs.org/headers
 
