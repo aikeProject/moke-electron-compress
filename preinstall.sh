0930a2a46058de8f3cd3314863d87ba43ee7b8aa
成雨 <rensailong@zhongan.io>
2020-01-06 17:39:08 +0800

    update

--- preinstall.sh
+++ preinstall.sh
@@ -38,16 +38,16 @@ fi
 
 # node-gyp 工具重新编译sharp，以适应当前版本electron
 # https://electronjs.org/docs/tutorial/using-native-node-modules#%E4%B8%BA-electron-%E6%89%8B%E5%8A%A8%E7%BC%96%E8%AF%91
-npm i node-gyp@5.0.7 -D -E
-
-npm i prebuild-install@5.3.3 -g -E
-
-npm i sharp@0.23.4 -D -E
-
-echo "重新编译sharp"
-cd node_modules/sharp
-echo "切换到sharp目录下: $(pwd)"
-echo "npx node-gyp rebuild --target=${electron} --arch=x64 --dist-url=https://electronjs.org/headers"
-#export HOME="~/.electron-gyp"
-npx node-gyp rebuild --target=${electron} --arch=x64 --dist-url=https://electronjs.org/headers
+#npm i node-gyp@5.0.7 -D -E
+#
+#npm i prebuild-install@5.3.3 -g -E
+#
+#npm i sharp@0.23.4 -D -E
+#
+#echo "重新编译sharp"
+#cd node_modules/sharp
+#echo "切换到sharp目录下: $(pwd)"
+#echo "npx node-gyp rebuild --target=${electron} --arch=x64 --dist-url=https://electronjs.org/headers"
+##export HOME="~/.electron-gyp"
+#npx node-gyp rebuild --target=${electron} --arch=x64 --dist-url=https://electronjs.org/headers
 
